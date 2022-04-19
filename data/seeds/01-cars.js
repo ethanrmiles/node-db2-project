9a6b134c86f1716b3dac0fd10369d802bcdcfcfb
Ethan Miles <ethanmilescs@gmail.com>
2022-04-19 15:14:29 -0500

    created a server INSERT helper function

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -8,6 +8,7 @@ exports.seed = async function(knex) {
   await knex('cars').insert([
     {vin: '123456789', make: 'bmw', model: '320i', mileage: 318000, title: 'clean', transmission: 'manual' },
     {vin: '987654321', make: 'bmw', model: '330ci', mileage: 190000, title: 'clean', transmission: 'manual' },
-    {vin: '246801357', make: 'bmw', model: '318is', mileage: 120000, title: 'salvage', transmission: 'manual' }
+    {vin: '246801357', make: 'bmw', model: '318is', mileage: 120000, title: 'salvage', transmission: 'manual' },
+    {vin: '888888888', make: 'bmw', model: '330i', mileage: 13000, title: 'clean', transmission: 'automatic' }
   ]);
 };
