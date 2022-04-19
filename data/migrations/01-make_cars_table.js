fdc530f070a1cd72f6f8cba663ecdefd93a0575e
Ethan Miles <ethanmilescs@gmail.com>
2022-04-19 14:28:40 -0500

    migration schema built

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    /* id */ tbl.increments(); 
+    /* vin */ tbl.text('vin').unique().notNullable()
+    /* make */ tbl.text('make').notNullable()
+    /* model */ tbl.text('model').notNullable()
+    /* mileage */ tbl.numeric('mileage').notNullable()
+    /* title */ tbl.text('title')
+    /* transmission */ tbl.text('transmission').notNullable()
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
