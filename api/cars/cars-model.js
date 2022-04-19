96e870567c388ffbeb3c18a5fa037f5ddfcc83b2
Ethan Miles <ethanmilescs@gmail.com>
2022-04-19 15:00:36 -0500

    api endpoint can fetch data from db!

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const knex = require('knex')
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = id => {
+  return db('cars').where('id', id).first()
 }
 
 const create = () => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -3,7 +3,13 @@ const Cars = require('./cars-model')
 const md = require('./cars-middleware')
 
 router.get('/', (req,res) => {
-
+    Cars.getAll()
+        .then(cars => {
+            res.json(cars)
+        })
+        .catch(err => {
+            res.status(500).json({ message: `failed to retreive cars ${err.message}`})
+        })
 })
 
 router.get('/:id', (req,res) => {
