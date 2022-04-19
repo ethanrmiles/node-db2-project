5e5ffb22ea8a030c44f2427351b308627e6e3d58
Ethan Miles <ethanmilescs@gmail.com>
2022-04-19 14:42:47 -0500

    server running

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+const md = require('./cars-middleware')
+
+router.get('/', (req,res) => {
+
+})
+
+router.get('/:id', (req,res) => {
+    
+})
+
+router.post('/', (req,res) => {
+    
+})
+
+module.exports = router;
